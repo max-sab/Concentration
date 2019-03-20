cf49ce4d3a6dabd7a43e8f5094e654cfdec529bc
Maksym Sabadyshyn <max.sabadyshyn@gmail.com>
2019-03-20 23:39:50 +0200

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Max Sabadyshyn on 3/20/19.
+//  Copyright © 2019 Maksym Sabadyshyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
