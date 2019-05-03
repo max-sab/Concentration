8e0be08473b25b9673ffe214bc2f9bd49acddab6
Maksym Sabadyshyn <max.sabadyshyn@gmail.com>
2019-05-03 23:40:04 +0300

    Homework one, 1/2 extra credit done. Based on dictionary

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -6,24 +6,20 @@
 //  Copyright © 2019 Maksym Sabadyshyn. All rights reserved.
 //
 
+/*
+  You can find out what time it is using the Date struct. Read the documentation to figure out how it works and then use it to adjust your scoring so that the more quickly moves are made, the better the user’s score is. You can modify the scoring Required Task in doing this, but the score must still somehow be dependent on matches being rewarded and mismatches of previously-seen cards being penalized (in addition to being time-based). It’s okay if a “good score” is a low number and a “bad score” is a high number.*/
+
 import UIKit
 
 class ViewController: UIViewController {
     lazy var game = Concetration(numberOfPairsOfCards: (cardButtons.count+1)/2)
-    var flipCount: Int = 0{
-        didSet{
-            flipCountLabel.text = "Flips: \(flipCount)"
-        }
-    }
-    
     
     @IBOutlet weak var flipCountLabel: UILabel!
-    
+    @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet var cardButtons: [UIButton]!
     
     @IBAction func touchCard(_ sender: UIButton) {
         if let cardNumber = cardButtons.index(of: sender){
-             flipCount+=1
              game.chooseCard(at: cardNumber)
              updateViewFromModel()
         } else{
@@ -31,30 +27,91 @@ class ViewController: UIViewController {
         }
     }
     
+    //emoji packs
+    
+    var dictionaryOfThemes: [String:[String]]! =
+                            ["emojiHalloween":["👻","🎃", "🍭","😈","🕷","🕸"],
+                            "emojiNewYear":["🥂","⛄️","❄️","🌲","🐉","🎅🏻"],
+                            "emojiIndependenceDay":["🇺🇸","🇺🇦","🗽","🇺🇳","👮🏻‍♂️","👨🏿‍💼"],
+                            "emojiValentinesDay":["💕","🌹","💑","🍷","💋","🥰"],
+                            "emojiMashaSamovol": ["👸🏽","😍","❤️","🥇","🍣","🐱"],
+                            "emojiStudyingDay":["🤓","🤔","👨🏼‍🏫","🏫","🚌","🎓"]]
+
+    var emoji = [Int:String]()
+    
+    lazy var currentEmojiThemeName = "emojiHalloween"
+    
+    var backgroundColor=#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    var cardsColor=#colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+
+    
     func updateViewFromModel(){
+        flipCountLabel.text = "Flips: \(game.numberOfFlips)"
+        scoreLabel.text = "Score: \(game.score)"
         for index in cardButtons.indices{
             let button = cardButtons[index]
             let card = game.cards[index]
             if card.isFaceUp{
                 button.setTitle(emoji(for: card), for: UIControlState.normal)
-                button.backgroundColor=#colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                button.backgroundColor=cardsColor
             } else{
                 button.setTitle("", for: UIControlState.normal);
-                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0) : cardsColor
             }
         }
       
     }
     
-    var emojiChoices = ["👻","🎃", "🍭","😈","🕷","🕸"]
-    
-    var emoji = [Int:String]()
-    
     func emoji(for card: Card)->String{
-        if emoji[card.identifier]==nil, emojiChoices.count>0{
-            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
-            emoji[card.identifier]=emojiChoices.remove(at: randomIndex)
+        
+        if emoji[card.identifier]==nil,dictionaryOfThemes[currentEmojiThemeName] != nil,dictionaryOfThemes[currentEmojiThemeName]!.count>0{
+            let randomIndex = Int(arc4random_uniform(UInt32(dictionaryOfThemes[currentEmojiThemeName]!.count)))
+            emoji[card.identifier]=dictionaryOfThemes[currentEmojiThemeName]!.remove(at: randomIndex)
         }
         return emoji[card.identifier] ?? "?"
     }
+    
+    @IBAction func newGameButtonPressed(_ sender: UIButton) {
+        game = Concetration(numberOfPairsOfCards: (cardButtons.count+1)/2)
+        dictionaryOfThemes =
+            ["emojiHalloween":["👻","🎃", "🍭","😈","🕷","🕸"],
+             "emojiNewYear":["🥂","⛄️","❄️","🌲","🐉","🎅🏻"],
+             "emojiIndependenceDay":["🇺🇸","🇺🇦","🗽","🇺🇳","👮🏻‍♂️","👨🏿‍💼"],
+             "emojiValentinesDay":["💕","🌹","💑","🍷","💋","🥰"],
+             "emojiMashaSamovol": ["👸🏽","😍","❤️","🥇","🍣","🐱"],
+             "emojiStudyingDay":["🤓","🤔","👨🏼‍🏫","🏫","🚌","🎓"]]
+        currentEmojiThemeName = Array(dictionaryOfThemes.keys)[Int(arc4random_uniform(UInt32(Array(dictionaryOfThemes).count)))]
+        initialViewSettings()
+        updateViewFromModel()
+    }
+    
+    func initialViewSettings(){
+        switch currentEmojiThemeName{
+        case "emojiNewYear":
+            backgroundColor=#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            cardsColor=#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+        case "emojiIndependenceDay":
+            backgroundColor=#colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+            cardsColor=#colorLiteral(red: 0.09019608051, green: 0, blue: 0.3019607961, alpha: 1)
+        case "emojiValentinesDay":
+            backgroundColor=#colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+            cardsColor=#colorLiteral(red: 0.9995340705, green: 0.988355577, blue: 0.4726552367, alpha: 1)
+        case "emojiMashaSamovol":
+            backgroundColor=#colorLiteral(red: 0.0201725252, green: 0.4219881296, blue: 0.02140678838, alpha: 1)
+            cardsColor=#colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+        case "emojiStudyingDay":
+            backgroundColor=#colorLiteral(red: 1, green: 0.8323456645, blue: 0.4732058644, alpha: 1)
+            cardsColor=#colorLiteral(red: 0.4620226622, green: 0.8382837176, blue: 1, alpha: 1)
+        default:
+            backgroundColor=#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            cardsColor=#colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+            
+        }
+        view.backgroundColor=backgroundColor
+        for index in cardButtons.indices{
+            cardButtons[index].backgroundColor = cardsColor
+        }
+    }
+    
 }
+
