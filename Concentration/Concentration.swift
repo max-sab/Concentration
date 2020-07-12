c0c1e1a8db26c72f70d2e0c05465139b4dd2d799
Maksym Sabadyshyn <max.sabadyshyn@gmail.com>
2020-07-12 11:49:27 +0300

    Concentration file refactored

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -8,106 +8,92 @@
 
 import Foundation
 
-struct Concentration{
-    private(set) var cards =  [Card]()////////////////
+struct Concentration {
+    
+    private(set) var cards =  [Card]()
     public var numberOfFlips = 0
     public var score = 0
     public var level = 1
     public var goToNextLevel = false
-    
-    private var indexOfOnlyFacedUpCard: Int?{
-        get{
-            return cards.indices.filter{cards[$0].isFaceUp}.oneAndOnly
-//            var onlyOneCardFacedUp: Int?
-//            for index in cards.indices{
-//                if cards[index].isFaceUp{
-//                    if onlyOneCardFacedUp==nil{
-//                        onlyOneCardFacedUp=index
-//                    } else{
-//                        return nil
-//                    }
-//                }
-//
-//            }
-//            return onlyOneCardFacedUp
+
+    private var indexOfOnlyFacedUpCard: Int? {
+        get {
+            return cards.indices.filter{ cards[$0].isFaceUp }.oneAndOnly
         }
         
-        set{
-            for index in cards.indices{
-                cards[index].isFaceUp = (index==newValue)
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = (index == newValue)
             }
         }
     }
     
-    mutating func chooseCard(at index: Int){
+    mutating func chooseCard(at index: Int) {
         assert(cards.indices.contains(index), "Concentration.chooseCard(at:\(index): no index like that in cards!")
         //changing Flips number but checking if card is aplicable
-        if cards[index].isMatched==false, cards[index].isFaceUp==false{
-           numberOfFlips+=1
+        if cards[index].isMatched == false, cards[index].isFaceUp==false {
+            numberOfFlips += 1
         }
+
         //if card hasn't been matched...
-        if !cards[index].isMatched{
+        if !cards[index].isMatched {
+
             //if there is one and only one faced up card and it's not the one we just opened...
-            if let matchedIndex = indexOfOnlyFacedUpCard, index != matchedIndex{
+            if let matchedIndex = indexOfOnlyFacedUpCard, index != matchedIndex {
+
                 //if its identifier equals to identifier of previously faced up card...
-                if cards[index] == cards[matchedIndex]{
+                if cards[index] == cards[matchedIndex] {
+
                     //Bingo!
-                    cards[index].isMatched=true
-                    cards[matchedIndex].isMatched=true
-                    score+=2
+                    cards[index].isMatched = true
+                    cards[matchedIndex].isMatched = true
+                    score += 2
                 }
                 //comes here either if cards were matched or if those chosen cards were unmatched
                 
                 //decreasing score if card was seen before and unmatched
-                if (cards[index].hasSeenBefore && !cards[index].isMatched && score>0){
+                if (cards[index].hasSeenBefore && !cards[index].isMatched && score > 0) {
                     score = score - 1
                 }
-                cards[index].isFaceUp=true
-                cards[index].hasSeenBefore=true
+                cards[index].isFaceUp = true
+                cards[index].hasSeenBefore = true
                 
-            //if there were no faced up cards or if we matched two earlier and they still on the screen
-            } else{
+                //if there were no faced up cards or if we matched two earlier and they still on the screen
+            } else {
                 indexOfOnlyFacedUpCard = index
             }
         }
         
         
         //substitution for the bottom
-        if cards.contains(where: {$0.isMatched==false}){
+        if cards.contains(where: { $0.isMatched == false }) {
             return
         }
-//        for i in cards.indices{
-//            if cards[i].isMatched == false{
-//                return
-//            }
-//        }
-        
-        
-        
+
         goToNextLevel = true
         level += 1
     }
     
-    init(numberOfPairsOfCards:Int){
-           assert(numberOfPairsOfCards>0, "init(numberOfPairsOfCards:\(numberOfPairsOfCards): number should be larger than zero!")
+    init(numberOfPairsOfCards:Int) {
+        assert(numberOfPairsOfCards>0, "init(numberOfPairsOfCards:\(numberOfPairsOfCards): number should be larger than zero!")
         for _ in 1...numberOfPairsOfCards{
             let card = Card()
             cards+=[card,card]
         }
         
         //shuffle cards using Fisher-Yates algorithm
-        for index in cards.indices{
+        for index in cards.indices {
             let randomIndex = Int(arc4random_uniform(UInt32(cards.count - index)))
             let tempCard = cards[index]
-            cards[index]=cards[randomIndex]
-            cards[randomIndex]=tempCard
+            cards[index] = cards[randomIndex]
+            cards[randomIndex] = tempCard
         }
         //cards.shuffle()
     }
 }
 
-extension Collection{
-    var oneAndOnly: Element?{
+extension Collection {
+    var oneAndOnly: Element? {
         return count == 1 ? first : nil
     }
 }
