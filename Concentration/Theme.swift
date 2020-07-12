c9604abe931b2f625a9aa61f813c40cde560faa1
Maksym Sabadyshyn <max.sabadyshyn@gmail.com>
2020-07-12 12:10:47 +0300

    Added Theme struct to have better architecture when it comes to the view

---
+++ Concentration/Theme.swift
@@ -0,0 +1,17 @@
+//
+//  Theme.swift
+//  Concentration
+//
+//  Created by Maksym Sabadyshyn on 7/12/20.
+//  Copyright © 2020 Maksym Sabadyshyn. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Theme {
+    let name: String
+    let emojiPackage: [String]
+    let backgroundColor: UIColor
+    let cardColor: UIColor
+}
