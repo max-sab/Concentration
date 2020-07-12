4c23068fafc91089038ff20830110698d33f047c
Maksym Sabadyshyn <max.sabadyshyn@gmail.com>
2020-07-12 11:58:46 +0300

    Code refactoring for popup ViewController made

--- Concentration/PopUpViewController.swift
+++ Concentration/PopUpViewController.swift
@@ -11,27 +11,14 @@ import UIKit
 class PopUpViewController: UIViewController {
 
     @IBOutlet weak var popUp: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         popUp.layer.cornerRadius = 24
         self.view.backgroundColor = UIColor.black.withAlphaComponent(0.75)
-
-        // Do any additional setup after loading the view.
     }
     
-
     @IBAction func closePopUp(_ sender: UIButton) {
-         
          self.view.removeFromSuperview()
     }
-     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
